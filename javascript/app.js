f1aa443df2cb210e33481775e294c55a5ecad8a0
olgakahraman <ol.khrmn@gmail.com>
2022-12-27 22:46:17 +0300

    added js updateCity function

--- javascript/app.js
+++ javascript/app.js
@@ -1,26 +1,49 @@
-function updateTime(){
+function updateTime() {
+  //New York
+  let newYorkElement = document.querySelector("#new-york");
+  if (newYorkElement) {
+    let newYorkDateElement = newYorkElement.querySelector(".date");
+    let newYorkTimeElement = newYorkElement.querySelector(".time");
+    let newYorkTime = moment().tz("America/New_York");
 
-//New York
-let newYorkElement = document.querySelector("#new-york");
-let newYorkDateElement = newYorkElement.querySelector(".date");
-let newYorkTimeElement = newYorkElement.querySelector(".time");
-let newYorkTime = moment().tz("America/New_York");
+    newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
+    newYorkTimeElement.innerHTML = newYorkTime.format(
+      "h:mm:ss[<small>]A[</small>]"
+    );
+  }
+  //Shenzhen
+  let shenzhenElement = document.querySelector("#shenzhen");
+  if (shenzhenElement) {
+    let shenzhenDateElement = shenzhenElement.querySelector(".date");
+    let shenzhenTimeElement = shenzhenElement.querySelector(".time");
+    let shenzhenTime = moment().tz("Hongkong");
 
-newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
-newYorkTimeElement.innerHTML = newYorkTime.format(
-  "h:mm:ss[<small>]A[</small>]"
-);
-
-//Shenzhen
-let shenzhenElement = document.querySelector("#shenzhen");
-let shenzhenDateElement = shenzhenElement.querySelector(".date");
-let shenzhenTimeElement = shenzhenElement.querySelector(".time");
-let shenzhenTime = moment().tz("Hongkong");
-
-shenzhenDateElement.innerHTML = shenzhenTime.format("MMMM Do YYYY");
-shenzhenTimeElement.innerHTML = shenzhenTime.format(
-  "h:mm:ss[<small>]A[</small>]"
-);
+    shenzhenDateElement.innerHTML = shenzhenTime.format("MMMM Do YYYY");
+    shenzhenTimeElement.innerHTML = shenzhenTime.format(
+      "h:mm:ss[<small>]A[</small>]"
+    );
+  }
 }
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML += `
+  <div class="city">
+        <div>
+          <h2>${cityName}</h2>
+          <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(
+    "A"
+  )}</small></div>
+      </div>
+    `;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
