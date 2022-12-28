853230a0ccaa231a4b64deec2c8cd7f2ccb0c709
olgakahraman <ol.khrmn@gmail.com>
2022-12-28 17:22:46 +0300

    citiesElement +

--- javascript/app.js
+++ javascript/app.js
@@ -32,7 +32,7 @@ if(cityTimeZone==="current"){
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = `
+  citiesElement.innerHTML += `
   <div class="city">
         <div>
           <h2>${cityName}</h2>
